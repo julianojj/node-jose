ab6952e1a1be1fad3da1b828af62b2ef9c6b589e
Juliano <cont.juliano@protonmail.com>
2021-12-19 09:49:27 -0300

    convert json to valid node-jose key

--- src/service/JWEService.js
+++ src/service/JWEService.js
@@ -1,11 +1,23 @@
+const { JWK } = require('node-jose')
 const { JWE } = require('node-jose')
 
-const jweEncrypt = (input, key) => {
-    return JWE.createEncrypt({format: 'compact'}, key)
+const convertToKey = (key) => {
+    return JWK.asKey(key, 'json')
+}
+
+const jweEncrypt = async (input, key) => {
+    const asKey = await convertToKey(key)
+    return JWE.createEncrypt({format: 'compact'}, asKey)
         .update(input)
         .final()
 }
 
+const jweDecrypt = async (input, key) => {
+    const asKey = await convertToKey(key)
+    return JWE.createDecrypt(asKey).decrypt(input)
+}
+
 module.exports = {
-    jweEncrypt
+    jweEncrypt,
+    jweDecrypt
 }
